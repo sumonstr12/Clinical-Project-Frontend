c9f8555996b4ef649a3bfa5bcfa7c6b11d0cc414
sumonstr12 <sumon220212@cseku.ac.bd>
2026-04-12 02:19:38 +0600

    Get request add in findDoctor.jsx to get Doctors instance and update myaxios.js

--- src/assets/utilities/myaxios.js
+++ src/assets/utilities/myaxios.js
@@ -5,17 +5,64 @@ const myaxios = axios.create({
     withCredentials: true
 });
 
+let isRedirecting = false;
+
+const logoutAndRedirect = () => {
+    if (isRedirecting) return; 
+    isRedirecting = true;
+
+    localStorage.removeItem("token");
+    window.location.href = "/";
+};
+
+
 myaxios.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
-
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
-
         return config;
     },
     (error) => Promise.reject(error)
+);
+
+
+myaxios.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            const refreshToken = Cookies.get("refresh_token");
+
+            
+            if (!refreshToken) {
+                logoutAndRedirect();
+                return Promise.reject(error);
+            }
+
+            
+            try {
+                const { data } = await axios.post(
+                    "http://localhost:8000/api/token/refresh/",
+                    { refresh: refreshToken }
+                );
+
+                localStorage.setItem("token", data.access);
+                originalRequest.headers.Authorization = `Bearer ${data.access}`;
+                return myaxios(originalRequest);
+
+            } catch (refreshError) {
+                
+                logoutAndRedirect();
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
 );
 
 export default myaxios;
