398e107b3ca433a00441094461a49bfd10e819a9
sumonstr12 <sumon220212@cseku.ac.bd>
2026-08-18 19:28:16 +0600

    Update SignUp page

--- src/assets/pages/general_user/SignUpPage.jsx
+++ src/assets/pages/general_user/SignUpPage.jsx
@@ -3,14 +3,10 @@ import SignUp from '../../components/general_user/SIgnUp'
 import Navbar from '../../partials/Navbar'
 import '../../css/general_user/SignUpCss.css'
 import Footer from '../../partials/Footer'
-// import '../../css/global_app.css'
 
 const SignUpPage = () => {
   return (
     <div>
-        {/* <div className="sign-up-page-section"><Navbar /></div>
-        <div className="sign-up-page-section"><SignUp /></div>
-        <div className="sign-up-page-section"><Footer /></div> */}
         <Navbar />
         <SignUp />
         <Footer />
